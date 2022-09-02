fd3145b5f0c7d8d5eb339730431a9f223bfc86d3
pmohanj <pmohan6712@gmail.com>
2022-09-02 22:52:19 +0530

    cleaned some code and added some rest apis

--- handlers/hello.go
+++ handlers/hello.go
@@ -15,10 +15,11 @@ func NewHello(l *log.Logger) *Hello {
 	return &Hello{l}
 }
 
+// ServeHTTP implements the go http.Handler interface
 func (h *Hello) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	h.l.Println("Hello World")
-	d, err := ioutil.ReadAll(r.Body)
 
+	d, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		http.Error(rw, "Oops", http.StatusBadRequest)
 		return
